5b54c8debaae4cdbc5200cacf47ab3d86f3c71a0
Christof <christof@myway.de>
2020-05-09 07:01:43 +0200

    added ordering to video_list view, still dirty

--- gatherer/views.py
+++ gatherer/views.py
@@ -8,20 +8,38 @@ from .models import Tag
 
 class VideoList(ListView):
     model = Video
+    filters = { 
+            "-published_at": "Datum ab",
+            "published_at": "Datum auf",
+            "-duration": "Dauer ab",
+            "duration": "Dauer auf",
+    }
+                
 
 
     def get_queryset(self):
         order_by = 'published_at'
+        if self.request.method == 'GET':
+            order_by = self.request.GET.get('order_by', order_by)
         if 'tag' in self.kwargs.keys():
             tag = self.kwargs['tag']
             qs = Video.objects.filter(tags__name=tag)
         else:
             qs = Video.objects.all()
-        qs.order_by(order_by)
+        qs = qs.order_by(order_by)
         return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         tags = Tag.objects.filter(video__isnull=False).distinct()
         context['tags'] = tags
+        context['filters'] = self.filters
+        order_by = 'published_at'
+        if self.request.method == 'GET':
+            order_by = self.request.GET.get('order_by', order_by)
+        context['order_by'] = order_by
+        tag = ''
+        if 'tag' in self.kwargs.keys():
+            tag = self.kwargs['tag']
+        context['current_tag'] = tag
         return context
